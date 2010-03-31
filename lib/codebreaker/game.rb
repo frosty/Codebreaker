ad5a96cad87da78305c516439a8ac05cb84b1c59
James Frost <james@jameswfrost.co.uk>
2010-03-31 13:11:19 +0100

    End of chapter 8.3

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -11,35 +11,9 @@ module Codebreaker
     end
     
     def guess(guess)
-      @output.puts '+'*exact_match_count(guess) + '-'*number_match_count(guess)
-    end
-    
-    def exact_match?(guess, index)
-      guess[index] == @secret[index]
-    end
-    
-    def number_match?(guess, index)
-      @secret.include?(guess[index]) && !exact_match?(guess, index)
-    end
-    
-    def exact_match_count(guess)
-      exact_match_count = 0
-      (0..3).map do |index|
-        if exact_match?(guess, index)
-          exact_match_count += 1
-        end
-      end
-      exact_match_count
-    end
-    
-    def number_match_count(guess)
-      number_match_count = 0
-      (0..3).map do |index|
-        if number_match?(guess, index)
-          number_match_count += 1
-        end
-      end
-      number_match_count
+      marker = Marker.new(@secret, guess)
+      @output.puts '+' * marker.exact_match_count +
+                   '-' * marker.number_match_count
     end
   end
 end
